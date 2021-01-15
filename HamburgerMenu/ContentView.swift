100099a6e4f6770620859607f1725f88eeea3ece
Joep Hinderink <hello@joephinderink.nl>
2021-01-15 14:47:48 +0100

    Initial Commit

---
+++ HamburgerMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HamburgerMenu
+//
+//  Created by Joep Hinderink on 15/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HamburgerMenu/HamburgerMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  HamburgerMenuApp.swift
+//  HamburgerMenu
+//
+//  Created by Joep Hinderink on 15/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct HamburgerMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
